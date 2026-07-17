92cb61e420de10f1f24ca6aa5a7cfb43829fd037
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-18 01:16:26 +0530

    Lecture 3). : - Arithmetic operators in Java with real life examples Revice.

--- Chap_1Basics.java/Arithmeticex.java
+++ Chap_1Basics.java/Arithmeticex.java
@@ -1,4 +1,7 @@
 public class Arithmeticex {
+
+    //Lecture 3). : - Arithmetic operators in Java with real life example Revice. 
+
     public static void main(String[] args){
 
         int yourSalary = 10000; 
@@ -7,14 +10,14 @@ public class Arithmeticex {
         int total = yourSalary + bonus;  // Addition
         System.out.println("total sum of your Salary is: " + total);
 
-        int deduction = 1000;
+        int deduction = 1000; //convience, tax, etc.
         int monthlyTotal = yourSalary - deduction;  //Substraction
         System.out.println("your monthly total is: " + monthlyTotal);
 
         int yearlyTotal = monthlyTotal * 12;     //Multiplication
         System.out.println("Your yearlytotal is: " + yearlyTotal);
 
-        int perChild = yearlyTotal / 3;  //Division
+        int perChild = yearlyTotal / 3;  //for education and other expenses //Division
         System.out.println("perChild distribution of salary is: " + perChild);
         
         int a = 5,b = 2,remainder;
