d3e7530bee7e45b3ea9439e212e8b5a7e171d925
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-19 22:31:59 +0530

    command line arguments

--- Chap_2ConditionalStatementAndLoops.java/Commandline.java
+++ Chap_2ConditionalStatementAndLoops.java/Commandline.java
@@ -1,4 +1,10 @@
 public class Commandline{
+
+    
+    // main method is the entry point of the program
+    // command line arguments are passed to the main method as an array of Strings
+
+
     public static void main(String args[]){ 
         /*on cmd type javac classname.java
         java classname Arguments//commandline arguments passed according to args[indexing fixed to your arguments printed] */
