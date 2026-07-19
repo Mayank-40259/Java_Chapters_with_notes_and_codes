208d5cbc91f02595d834753bb9ec8b84ea880dfd
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-19 22:41:35 +0530

    Lecture 8 : Pattern Printing in Java with examples.

--- Chap_2ConditionalStatementAndLoops.java/patternprinting/Startriangle.java
+++ Chap_2ConditionalStatementAndLoops.java/patternprinting/Startriangle.java
@@ -1,5 +1,14 @@
 public class Startriangle {
-    public static void main(String[] args){
+
+    //Lecture 8 : Pattern Printing in Java with examples.
+    
+    // Nested-loop s are used to print patterns in Java. 
+    // A nested loop is a loop inside another loop. The outer loop is 
+    // responsible for the number of rows, while the inner loop is responsible 
+    // for the number of columns.
+
+
+     public static void main(String[] args){
     for(int i = 1; i <= 6; i++){
         for(int j = 1; j <= i; j++){
             System.out.print("* ");
