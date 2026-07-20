8621cce545e974b1af84213ed5c4085ff770c065
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-20 20:19:20 +0530

    Lecture 15). :- Packages in Java with Classpath.

---
+++ Chap_4Arrays_in_Java/Packages.java
@@ -0,0 +1,15 @@
+public class Packages {
+
+    // Lecture 15). :- Packages in Java with Classpath.
+
+    //package : it is also known as folders.
+    //different different packages ke under different different classes hai.to differentiate classes.
+    //why we create packages ? --> naam me distingtion aa jata hai.sturcture and organized ways or collection of files and folders that is called package.all things are cleared.
+    //pakages : package(folders)-->subpackage(sub folders)-->files as class(in collections).
+    // 
+    //classpath :jvm finding class(test.class) in cmd code javac test.java java test.-->not execute but execute by 1). as file : javac test.java java Test --> hello world. 2).as folder javac test(package he test)/Test(test package ke ander class hai Test).
+    //java.lang package automatically import ho jaata hai.String example.
+    //java.util.list ---> not automatically import we write/explicitily write karte hai  ---> list.in Arraylist.and util.Scanner example.
+    //Syntax of package write is : rootpackage --> reversemaindomainnamethenprojectname ---> com.engineeringdigest.corejava.
+    
+}
