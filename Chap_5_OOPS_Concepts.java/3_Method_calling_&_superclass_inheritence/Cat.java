211970a76604d4f0147f57e3df86ed143ace8a76
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-21 22:18:02 +0530

    understand cat class example.

---
+++ Chap_5_OOPS_Concepts.java/3_Method_calling_&_superclass_inheritence/Cat.java
@@ -0,0 +1,24 @@
+public class Cat extends Animal{ //access all properties of Animal Class or Parent class.and Cat is a child class.
+
+    //Lecture 03). b).Cat.java class
+    // understand cat class example. 
+
+    String breed;
+    String name;
+
+    public Cat(){ //default constructor.
+        this.name = name;
+        System.out.println(name);
+    }
+    public void makeSound(){
+       System.out.println("Meow!");
+    }
+    public void breed(){
+        System.out.println("rat");
+    }
+    public void name(){
+        System.out.println("Bob");
+
+    }
+
+}
