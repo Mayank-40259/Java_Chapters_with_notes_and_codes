13e585bd8c9fabd36f722bcff909dcab917666c0
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-21 22:18:52 +0530

    understand dog class example.

---
+++ Chap_5_OOPS_Concepts.java/3_Method_calling_&_superclass_inheritence/Dog.java
@@ -0,0 +1,24 @@
+public class Dog extends Animal {
+    
+    //Lecture 03). c).Dog.java class 
+    // understand dog class example.
+
+    String name;
+    String breed;
+
+    public Dog(){ //default constructor.
+        this.name = name;
+        System.out.println(name);
+    }
+    public void makeSound(){
+        System.out.println("Bhow-Bhow!");
+    }
+    public void breed(){
+        System.out.println("Dog Biscuits");
+    }
+    public void name(){
+        System.out.println("Tommy & Bruno");
+    }
+
+
+}
