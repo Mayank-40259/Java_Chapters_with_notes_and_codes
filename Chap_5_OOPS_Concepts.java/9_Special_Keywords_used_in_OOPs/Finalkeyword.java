1f8fa10d59c1eaba828e83c2f9f6c1d06ff9397b
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-24 22:26:24 +0530

    Example --> Lec.09). --> Special Keyword in OOPs.

---
+++ Chap_5_OOPS_Concepts.java/9_Special_Keywords_used_in_OOPs/Finalkeyword.java
@@ -0,0 +1,21 @@
+package Special_Keywords_used_in_OOPs;
+public class Finalkeyword {
+
+    //how we apply Final Keyword on 1).Method 2).Variable 3).Class 
+    //in intellij idea finalkeyword vechicles class insided car,cycle,vehicle classes are present.
+    //Final keyword ko variables ko defination and declaration me use karte hai taki dobara initialization na karna pade taki value change na ho. example .pi wala = 3.14159.
+    //Final keyword ko methods ke sath use kr sakte hai unko constant banane ke liye taki koi dusri class ka method override na kr sake class extends ke sath me. example hai Air bags in cars to EVCar.
+    //Final keyword ko class ke sath likkne me kiya jata hai taki koi dusri class extends na kr sake. example hai final keyword used hua hai car class ke sath me evcar class extend kr rahi thi usse extend hatwaya tha.
+    //kya final keyword ko constructor ke sath use kr sakte hai kya? 
+    //working of constructor. to initialization state of object. constructor(ek tarike ka function or method hi toh hai) kb call hota hai jb ek instance class ka initialize hota hai as Student student = new Student(call ho raha hai constructor.).
+    //Final ka use kya hai --> jb hm me method pe inheritance,overriding, ko rokna ho lekin constructor to inherit hote hi nahi hai. to koi matlab hi nahi banta hai final bannane ka. to hm use nahi karte hai final ko constructor ke beech me.(modifier final not allowed here.)
+
+    public static void main(String[] args) {
+
+        System.out.println(" Hello World ");
+
+    }
+
+}
+
+
