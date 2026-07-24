8d8c83e23bac7fb7578930af3c5aa35322e6c2f1
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-07-24 22:19:59 +0530

    Lecture 08). :- Polymorphism. ,Calculator.java example.

---
+++ Chap_5_OOPS_Concepts.java/8_Polymorphism_&_its_Types/Polymorphishm.java
@@ -0,0 +1,16 @@
+public class Polymorphishm {
+
+
+    //Lecture 08). :- Polymorphism. ,Calculator.java example.
+
+    //Polymorphishm : it is a core concept in object oriented programming (OOPS) that allows
+    //method to do different things based on the object it is acting upon,even though the
+    //method name and its signature might be the same.
+
+    //eg:  object(anywhere)object kon hai uske base pr different act karega method.method()-->methode me signature same honge tb bhi different kaam karega.
+    //there are two-(2)-types of polymorphishm 1).compile-time-polymorphishm & 2).run-time-polymorphishm.
+    //eg : for compile time polymorphism --> (Calculator.java).--> method overloading
+    //eg : for run time polymorphishm --> in intellij Idea (Polymorphishm package create).with help of inheritance --> method overriding. --> dynamic method dispatch.
+
+    
+}
