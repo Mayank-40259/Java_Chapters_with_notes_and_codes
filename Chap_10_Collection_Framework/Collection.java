c61183562e1c4b68f16ea9ed9f232d8b73293993
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-08-25 22:27:29 +0530

    Collection Introduction Lecture.01.

---
+++ Chap_10_Collection_Framework/Collection.java
@@ -0,0 +1,56 @@
+public class Collection{
+
+
+    // Collection:- A COllection is simply an object that represents 
+    //       a group of objects, known as its elements.
+    // eg:- collection of coins and books 
+    // 
+    // 
+    // Collection Framework :- it provides a set of interfaces and 
+    //      classes that help in managing groups of objects.
+    // 
+    // --> Before the introduction of the Collection Framework in JDK 1.2, 
+    //      Java used to rely on a variety of classes like Vector,Stack,Hashtable & 
+    //         arrays to store and manipulate groups of objects.
+    // 
+    // --> However, these classes had several drawbacks.
+    // 
+
+
+    // 
+    // ##Problems created hui --> to solve these problems, the Collection Framework was introduced.
+    //  
+    //1).Inconsistency:- Each class had a different way of managing collections, 
+    //        leading to confusion  and a steep learning curve.
+    // 
+
+
+    //2).Lack of inter-operability: These classes were not designed to work
+    //            together seamlessly.
+    // 
+
+
+    //3).No common interface:- There was no common interface for all these classes,
+    //    which meant you couldn't write generic algorithms that could operate 
+    //   an different types of collections.
+    // 
+
+
+    // 
+    // Unified architecture:- A consistent set of interfaces for all collections.
+    // Inter-operability:- Collections can be easily interchanged and manipulated in a uniform way.
+    // Reusability:- Generic algorithms can be written that work with any collection.
+    // Efficiency:- The fromework provides efficient algorithms for basic operations like searching,
+    //       sorting, and manipulation.
+    // 
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!...");
+        //Collection Introduction Lecture.01.
+        
+    }
+}
