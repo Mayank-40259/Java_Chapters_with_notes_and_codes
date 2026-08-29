dcae805b78c49c8f6dd2e3302722ccd013bc948a
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-08-29 11:06:55 +0530

    ArrayList Methods and its implementation

--- Chap_10_Collection_Framework/Collection.java
+++ Chap_10_Collection_Framework/Collection.java
@@ -66,8 +66,10 @@ public class Collection{
     //                                |
     //                            Collection
     //                                |
-    //          -------------------------------------------------------
-    //          |                     |                               |
+    //          --------------------------------------------------------
+    //          | (List)              | (Sets)                         | (Queue)
+    //          |                     |                                |
+    //          ^                     ^                                ^  
     //      ArrayList               HashSet                      LinkedList          ---> ArrayDeque
     //      LinkedList            LinkedHashSet                 PriorityQueue       /
     //        Vector                TreeSet                        Deque -------------->LinkedList
@@ -88,14 +90,75 @@ public class Collection{
     // 
 
 
-    // --> 1).List :- the List interface in Java is a part of the java.util package 
+    // ##Collection Interfaces :-  
+    // --> 1st part).List :- the List is an interface in Java is a part of the java.util package 
     //         and is a sub-interface of the Collection Interface. It provides a way to 
     //        "store an ordered collection of elements"(known as a sequence). Lists allow for 
     //        precise control over where elements inserted and "can contain duplicate elements." 
     // 
-    // -->
-    // -->
-    // -->
+    // ->List extend karta hai Collection class ko.
+    // -> The List interface is implemented by several classes in the Java Collection Framework,
+    //    such as ArrayList, LinkedList, Vector, and Stack.
+    // 
+    // #Key Features of the List Interface :-
+    //      Order Preservation
+    //      Index-Based Access
+    //      Allows Duplicates
+    //  
+    // 
+    // --> 1).ArrayList :- An ArrayList is a resizeable array implementation of 
+    //           the List interface. Unlike arrays in Java, which have a fixed size, 
+    //         an "ArrayList can change its size dynamically" as elements are added or removed. 
+    //        This flexibility makes it a popular choice when the number of elements in a list isn't known in advance.
+    // ex:- code of ArrayList in intellijIdea --> Lecture-01 --> main.java --> ArrayList code.
+    // 
+    // -> #Internal Working of Arraylist :- 
+    //        Unlike a regular array, which has a fixed size, 
+    //        an ArrayList can grow and shrink as elements are added or removed. This dynamic 
+    //       resizing is achieved by creating a new array when the current array is full and copying 
+    //       the elements to the new array.
+    // 
+    // -> When you create an Arraylist, it has an initial capacity(default is 10). 
+    //    The capacity refers to the size of the internal array
+    //     that can hold elements before needing to resize.
+    // 
+    //    #Method-01 
+    //   #Adding Elements :-
+    //  When we add an element to an ArrayList, the following steps occurs :-
+    //  #1).Check Capacity :- 
+    //              Before adding the new element, Arraylist checks if there is 
+    //              enough space in the internal array(elementData).if the array is full, 
+    //              it needs to be resized.
+    // #2).Resize if Necessary :- 
+    //            if the internal array is full, the ArrayList will create
+    //            a new array with a larger capacity (usually 1.5 times the current capacity)
+    //            and copy the elements from the old array to the new array.
+    // #3).Add the Element :-
+    //         The new element is then added to the internal array at 
+    //         the appropriate index, and the size is incremented.
+    // 
+    //    #Method-02
+    //   #Resizing the Array :-
+    //  1).Initial Capacity :- By default, the initial capacity is 10. This means 
+    //                       the internal array can hold 10 elements before it needs to grow.
+    // 
+    //  2).Growth Factor :- When the internal array is full, a new array is created 
+    //                     with a size 1.5 times the old array. This growth factor 
+    //                   balances memory efficiency and resizing cost.
+    // 
+    //  3).Copying Elements :- When resizing occurs, all elements from the old
+    //                        array are copied to the new array, which is an O(n) operation,
+    //                   where n is the number of elements in the ArrayList.
+    //   #Method-03
+    //  #Removing Elements :- 
+    //   1).Check Bounds :- The ArrayList first checks if the index is within the valid range.
+    //   2).Removing the Element :- The element is removed, and all elements to the 
+    //           right of the removed element are shifted one position to the left to fill the gap.
+    //   3).Reduce Size :- The size is decremented by 1.
+    // 
+    // 
+    // --> 
+    // --> 
 
 
 
